574fc9ad386735a817977f3a2674963115d12aa2
adamhutchings <adam.abahot@gmail.com>
2022-12-27 20:52:13 -0800

    Add boilerplate GLFW code

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,69 @@
 /* Main file for Sniper program. */
 
+#include <chrono>
 #include <iostream>
+#include <thread>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+namespace sniper {
+
+GLFWwindow * wn;
+
+void init() {
+
+    glfwInit(); // TODO check for error
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    wn = glfwCreateWindow(500, 500, "Sniper", nullptr, nullptr);
+	glfwMakeContextCurrent(wn);
+	gladLoadGL();
+	glfwShowWindow(wn);
+
+}
+
+void mainloop() {
+
+    while (!glfwWindowShouldClose(wn)) {
+
+		// We want each frame to last for exactly 1/50th second,
+		// so capture the starting time so we can sleep for the
+		// needed amount of time at the end of the frame.
+		auto start_of_frame = std::chrono::steady_clock::now();
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+        // TODO rendering
+
+		glfwSwapBuffers(wn);
+		glfwPollEvents();
+
+		std::this_thread::sleep_until(start_of_frame + std::chrono::milliseconds(20));
+
+	}
+
+}
+
+void cleanup() {
+
+    glfwDestroyWindow(wn);
+	glfwTerminate();
+
+}
+
+}
 
 int main() {
-    std::cout << "Hello, World!\n";
+    
+    sniper::init();
+    sniper::mainloop();
+    sniper::cleanup();
+
     return 0;
+
 }
