54b1e93704b65900e8ce999dbf03d2b16fdf9bd8
adamhutchings <adam.abahot@gmail.com>
2023-01-03 12:43:58 -0800

    Add get/set for velocity

--- src/game/player.hpp
+++ src/game/player.hpp
@@ -1,15 +1,21 @@
 #pragma once
 
+#include <vector>
+
 #include <game/gameobject.hpp>
 #include <game/renderable.hpp>
 
 namespace game {
 
-class Player : GameObject, Renderable {
+class Player : public GameObject, Renderable {
 
   public:
     void render() override;
     Player(int xloc, int yloc);
+
+    // Read and update velocities.
+    std::pair<float, float> getvel();
+    void setvel(std::pair<float, float> newvel);
 
 };
 
