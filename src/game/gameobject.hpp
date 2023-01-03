984ec80b6d694e73cc74845c485f6fa1867c1a50
adamhutchings <adam.abahot@gmail.com>
2023-01-03 12:44:18 -0800

    Add position update code

--- src/game/gameobject.hpp
+++ src/game/gameobject.hpp
@@ -3,6 +3,8 @@ namespace game {
 class GameObject {
   public:
     void set_pos(int x_pos, int y_pos);
+    // This is JUST for updating the position because of velocity.
+    void do_pos_updates();
 
   protected:
     float x, y;
