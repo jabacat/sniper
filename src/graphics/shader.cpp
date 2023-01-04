2bc5b034a26f6e371820a1f98ef27c8b5cd1da44
adamhutchings <adam.abahot@gmail.com>
2023-01-04 16:49:06 -0500

    Fix shader code to work on macOS

--- src/graphics/shader.cpp
+++ src/graphics/shader.cpp
@@ -9,11 +9,39 @@
 
 namespace gl {
 
+#ifndef __APPLE__
 static std::string_view bytes_as_string_view(const std::vector<unsigned char>& vec) {
     return std::string_view(reinterpret_cast<const char*>(vec.data()), vec.size());
 }
+#else
+// Load a whole file as a string.
+const GLchar* load_file_as_string(const char* path) {
+    // TODO - do this with C++ classes.
+    FILE* fp = fopen(path, "r");
+    if (!fp) return nullptr;
+    fseek(fp, 0, SEEK_END);
+    int len = ftell(fp);
+    // Yes, I know the memes. Haha. Funny.
+    char* code = (char*) malloc(len + 1);
+    rewind(fp);
+    for (int pos = 0; pos < len; ++pos) {
+        code[pos] = getc(fp);
+    }
+    fclose(fp);
+    code[len] = 0;
+    return code;
+}
+#endif // __APPLE__
 
-Shader::Shader(std::string_view vertex_source, std::string_view fragment_source) {
+Shader::Shader(ZStringView vertex_fname, ZStringView fragment_fname) {
+
+#ifndef __APPLE__
+    std::vector<unsigned char> vertex_source = load_file_as_bytes(vertex_fname);
+    std::vector<unsigned char> fragment_source = load_file_as_bytes(fragment_fname);
+#else
+    auto vertex_source = load_file_as_string(vertex_fname);
+    auto fragment_source = load_file_as_string(fragment_fname);
+#endif // __APPLE__
 
     this->program_id = glCreateProgram();
     // TODO - check for error
@@ -22,23 +50,28 @@ Shader::Shader(std::string_view vertex_source, std::string_view fragment_source)
     int frag = create_subshader(fragment_source, GL_FRAGMENT_SHADER);
 
     this->link(vert, frag);
+
+#ifdef __APPLE__
+    free((void *) vertex_source);
+    free((void *) fragment_source);
+#endif // __APPLE__
+
 }
 
-Shader Shader::load_from_file(ZStringView vertex_fname, ZStringView fragment_fname) {
-    std::vector<unsigned char> vertcode = load_file_as_bytes(vertex_fname);
-    std::vector<unsigned char> fragcode = load_file_as_bytes(fragment_fname);
-    return Shader(bytes_as_string_view(vertcode), bytes_as_string_view(fragcode));
-}
-
-int Shader::create_subshader(std::string_view source, GLenum type) {
+int Shader::create_subshader(FileContents source, GLenum type) {
 
     int id = glCreateShader(type);
     if (!id) {
         // TODO -- log some error
     }
 
-    const char* src = source.data();
+#ifndef __APPLE__
+    GLchar const * src = source.data();
     int len = source.size();
+#else
+    GLchar const * src = source;
+    int len = strlen(source);
+#endif // __APPLE__
     glShaderSource(id, 1, &src, &len);
     glCompileShader(id);
 
@@ -101,9 +134,9 @@ void Shader::set_uniform_value(std::string name, float value) {
 }
 
 void load_all_shaders() {
-    GAME_SHADER = std::make_unique<Shader>(Shader::load_from_file(
+    GAME_SHADER = std::make_unique<Shader>(
         "src/shader/basic_vert.glsl", "src/shader/basic_frag.glsl"
-    ));
+    );
 }
 
 void unload_all_shaders() {
