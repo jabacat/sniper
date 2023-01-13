24f410483aac3b2941312b9c945329434380737e
Jake <jakeroggenbuck2@gmail.com>
2023-01-13 14:07:38 -0800

    Add rotation check

--- src/game/player.cpp
+++ src/game/player.cpp
@@ -1,4 +1,5 @@
 #include "player.hpp"
+#include <cmath>
 
 #include <graphics/texture.hpp>
 
@@ -11,10 +12,12 @@ Player::Player(int xloc, int yloc) {
     this->y = yloc;
     this->vx = 0;
     this->vy = 0;
+    this->angle;
 }
 
 void Player::render() {
-    tex::PLAYER_TEX->render(this->x, this->y, PLAYER_SIZE, PLAYER_SIZE, tex::RenderBasis::MID, tex::RenderBasis::MID);
+    tex::PLAYER_TEX->render(this->x, this->y, PLAYER_SIZE, PLAYER_SIZE,
+                            tex::RenderBasis::MID, tex::RenderBasis::MID);
 }
 
 std::pair<float, float> Player::getvel() {
@@ -24,6 +27,31 @@ std::pair<float, float> Player::getvel() {
 void Player::setvel(std::pair<float, float> newvel) {
     this->vx = newvel.first;
     this->vy = newvel.second;
+
+    const float PI = 3.14159265;
+    const float DEG_CONV = 180.0 / PI;
+
+    float x, y, m;
+    x = fabs(this->vx);
+    y = fabs(this->vy);
+
+    // Normalize the vector (x, y)
+    float r = sqrt(y * y + x * x);
+    x /= r;
+    y /= r;
+
+    // arccos approximation [-1, 1]
+    // m = -1.4 * x + 1.55;
+
+    // arccos approximation [0, 1]
+    // This one will be used because x is in the domain [0, 1]
+    // err = integral 0->1 arccos(x) - f(x) dx = -0.01
+    m = -1.12 * x + 1.57;
+    m *= DEG_CONV;
+
+    setangle(m);
 }
 
-}
+void Player::setangle(float a) { this->angle = a; }
+
+} // namespace game
